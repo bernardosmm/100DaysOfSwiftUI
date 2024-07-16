20f5b0c694e98f66ed8a89ab05d0e8793ee49480
bernardosmm <bernardosmm2004@gmail.com>
2024-07-16 13:43:26 -0300

    Checkpoint7

--- 100DaysOfSwiftUI/Checkpoints/Checkpoint7.swift
+++ 100DaysOfSwiftUI/Checkpoints/Checkpoint7.swift
@@ -8,6 +8,75 @@
 import Foundation
 class Checkpoint7: day {
     static func main() {
-        <#main method#>
+        class Animal {
+            var legs: Int
+
+            init(legs: Int) {
+                self.legs = legs
+            }
+        }
+
+        class Dog: Animal {
+            func speak() {
+                print("Woof! Woof!")
+            }
+        }
+
+        class Cat: Animal {
+            var isTame: Bool
+
+            init(legs: Int, isTame: Bool) {
+                self.isTame = isTame
+                super.init(legs: legs)
+            }
+
+            func speak() {
+                print("Miau, miau, miau, miau")
+            }
+        }
+
+        class Corgi: Dog {
+            override func speak() {
+                print("Corgi: Woof! Woof!")
+            }
+        }
+
+        class Poodle: Dog {
+            override func speak() {
+                print("Poodle: Yap! Yap!")
+            }
+        }
+
+        class Persian: Cat {
+            override func speak() {
+                print("Persian: Purr... Meow!")
+            }
+            
+            init(isTame: Bool) {
+                super.init(legs: 4, isTame: isTame)
+            }
+        }
+
+        class Lion: Cat {
+            override func speak() {
+                print("Lion: Roar!")
+            }
+            
+            init() {
+                super.init(legs: 4, isTame: false)
+            }
+        }
+        
+        let corgi = Corgi(legs: 4)
+        corgi.speak()
+
+        let poodle = Poodle(legs: 4)
+        poodle.speak()
+
+        let persian = Persian(isTame: true)
+        persian.speak()
+
+        let lion = Lion()
+        lion.speak()
     }
 }
